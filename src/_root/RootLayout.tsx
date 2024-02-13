fd0b3f9b808d8cbcdffbdc01d5afa200186a5770
Viraj Prajapati <prajapativiraj800@gmail.com>
2024-02-13 11:25:02 +0530

    Full Viraj_Gram is done.

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,7 +1,8 @@
+import { Outlet } from "react-router-dom";
+
 import Bottombar from "@/components/shared/Bottombar";
 import LeftSidebar from "@/components/shared/LeftSidebar";
 import Topbar from "@/components/shared/Topbar";
-import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
   return (
