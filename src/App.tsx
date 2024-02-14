28b19403b6b2a9277031342b9bfc3e5a6a8a6dae
Viraj Prajapati <prajapativiraj800@gmail.com>
2024-02-14 16:15:33 +0530

    checking

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./context/AuthContext";
 import { QueryProvider } from "./lib/react-query/QueryProvider";
