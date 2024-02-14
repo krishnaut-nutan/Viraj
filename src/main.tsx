a0955cf36e9b254fcf50298bfa73b997ff7b14c5
Viraj Prajapati <prajapativiraj800@gmail.com>
2024-02-14 15:25:53 +0530

    checking

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, } from "react-router-dom";
 
 import App from "./App";
 import AuthProvider from "./context/AuthContext";
@@ -14,11 +15,13 @@ window.addEventListener("focus", () => {
 });
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <QueryProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </QueryProvider>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <QueryProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
